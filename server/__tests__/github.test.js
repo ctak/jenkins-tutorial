9a520834b399054bde7514032d3538ea869c4d6b
DESKTOP-4D9GC9M\koung <koungs1.kim@gmail.com>
2021-02-18 16:41:58 +0900

    add GITHUB_ACCESS_TOKEN

--- server/__tests__/github.test.js
+++ server/__tests__/github.test.js
@@ -1,12 +1,11 @@
 const GitHub = require('../lib/github')
 
-// bdaa81233676600cbf073bfd81f3afc7806584ba
 describe('Integration with GitHub API', () => {
   let github
 
   beforeAll(() => {
     github = new GitHub({
-      accessToken: process.env.ACCESS_TOKEN,
+      accessToken: process.env.GITHUB_ACCESS_TOKEN,
       baseURL: 'https://api.github.com',
     })
   })
